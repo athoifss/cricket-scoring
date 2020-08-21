d5e7eed51ef94f5a011099dee69487e427e916ab
athoifss <atif_hossain@live.com>
2020-08-21 20:13:51 +0530

    remaining files for last commit

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const jwt = require("jsonwebtoken");
-const path = require("path");
-const initDb = require("./helper/db").initDb;
-const getDb = require("./helper/db").getDb;
+const initDb = require("./api/helpers/db").initDb;
 
 const app = express();
+const port = 5000;
+const route = require("./api/routes");
 
 app.use((req, res, next) => {
-  console.log(`request for ${req.url}`);
+  console.log(`${req.method} request for ${req.url}`);
   next();
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/", route);
 
-app.post("/user/add", (req, res, next) => {
-  console.log(req.body);
-  const dbo = getDb();
-  let newData = req.body;
-  dbo.collection("user").insertOne(newData, (error, result) => {
-    if (error) {
-      console.log(err);
-      res.status(500).json({
-        message: "Could not add new record",
-      });
-    } else {
-      res.status(200).json({
-        message: "User added",
-      });
-    }
-  });
-});
-
-const port = 5000;
 initDb(function (err) {
   app.listen(port, function () {
     console.log(`Started server on port ${port}`);
